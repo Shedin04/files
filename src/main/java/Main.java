57d16ff3e9d4de6d588f3388b8a3949d9405f783
New Sheriff in Town <dimashedin51@gmail.com>
2021-08-20 15:43:44 +0300

    add csv

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
 
 public class Main {
@@ -7,8 +6,25 @@ public class Main {
     public static void main(String[] args) throws IOException {
         int[] mas = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        writeMasToTxt(mas, "D:\\JAVA PROJECTS\\files\\src\\main\\data.txt");
-        writeMasToCsv(mas, "D:\\JAVA PROJECTS\\files\\src\\main\\data.csv");
+        /*write*/
+        /*writeMasToTxt(mas, "D:\\JAVA PROJECTS\\files\\src\\main\\data.txt");
+        writeMasToCsv(mas, "D:\\JAVA PROJECTS\\files\\src\\main\\data.csv");*/
+
+        /*read*/
+        double[] getmas = readToDouble("D:\\JAVA PROJECTS\\files\\src\\main\\data.csv");
+        System.out.println(Arrays.toString(getmas));
+    }
+
+    private static double[] readToDouble(String name) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(name));
+        String[] str = br.readLine()
+                .replaceAll(",",".") // заменяем запятые на точки
+                .split(";"); // разделяем элементы
+        double[] mas = new double[str.length];
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = Double.parseDouble(str[i]);
+        }
+        return mas;
     }
 
     private static void writeMasToTxt(int[] mas, String name) throws IOException {
