3fbfd9d09314ed0c8d9eecb7bf7b500bc6286878
New Sheriff in Town <dimashedin51@gmail.com>
2021-08-20 14:43:45 +0300

    add csv

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,14 +5,27 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        int [] mas = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] mas = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        FileWriter fr = new FileWriter("D:\\JAVA PROJECTS\\files\\src\\main\\data.txt");
-        fr.write(Arrays.toString(mas) + " | ");
+        writeMasToTxt(mas, "D:\\JAVA PROJECTS\\files\\src\\main\\data.txt");
+        writeMasToCsv(mas, "D:\\JAVA PROJECTS\\files\\src\\main\\data.csv");
+    }
+
+    private static void writeMasToTxt(int[] mas, String name) throws IOException {
+        FileWriter fr = new FileWriter(name);
+        fr.write(Arrays.toString(mas) + " | "); // нормально запишет
         fr.flush(); // промежуточная запись в файл
         for (var el: mas) {
-            // fr.write(el);// запишет в АСКИ
-            fr.write(el + " "); // запишет нормально, ибо подумает, что строка
+            // fr.write(el); // запишет в АСКИ
+            fr.write(el + ";");
+        }
+        fr.close(); // без close не запишется, будет в RAMe
+    }
+
+    private static void writeMasToCsv(int[] mas, String name) throws IOException {
+        FileWriter fr = new FileWriter(name);
+        for (var el : mas) {
+            fr.write(el + ";");
         }
         fr.close(); // без close не запишется, будет в RAMe
     }
